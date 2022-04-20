8eee98bc3cab8d2b56e0345e23e3aaafc6aa04e1
Superdan56 <71519345+Superdan56@users.noreply.github.com>
2022-04-20 18:27:28 -0400

    dialog progression complete

---
+++ Final-DnD/Assets/Scripts/ContinueButton.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ContinueButton : MonoBehaviour {
+
+    private int TextCounter;
+
+    public GameObject Text1;
+    public GameObject Text2;
+    public GameObject Text3;
+    public GameObject backGround;
+
+    void Start() {
+        TextCounter = 0;
+
+        Text1.SetActive(true);
+        Text2.SetActive(false);
+        Text3.SetActive(false);
+    }
+
+    void Update() {
+        
+        if (TextCounter == 1 ) {
+            Text1.SetActive(false);
+            Text2.SetActive(true);
+        }
+        else if (TextCounter == 2) {
+            Text2.SetActive(false);
+            Text3.SetActive(true);
+        }
+        
+        else if (TextCounter == 3) {
+            Text3.SetActive(false);
+            gameObject.SetActive(false);
+            backGround.SetActive(false);
+        }
+
+    }
+
+    public void Continue() {
+        TextCounter += 1;
+    }
+}

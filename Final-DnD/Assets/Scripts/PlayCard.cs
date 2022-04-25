45c3b26c83ede040f5476f9dc588f89f66517b3d
Superdan56 <71519345+Superdan56@users.noreply.github.com>
2022-04-25 14:27:36 -0400

    other text updates finished as well

--- Final-DnD/Assets/Scripts/PlayCard.cs
+++ Final-DnD/Assets/Scripts/PlayCard.cs
@@ -10,20 +10,28 @@ public class PlayCard : MonoBehaviour {
     public GameObject Card3;
     public GameObject Card4;
 
-    public Text health;
-    public Text enemy;
-    public Text actions;
+    public GameObject health;
+    public GameObject enemy;
+    public GameObject actions;
+    public GameObject discard;
 
     private int numActions;
+    private int discardpile;
 
     // Start is called before the first frame update
     void Start() {
         numActions = 3;
+        discardpile = 0;
+        actions.GetComponent<TMPro.TextMeshProUGUI>().text = "actions: " + numActions;
+        discard.GetComponent<TMPro.TextMeshProUGUI>().text = "Discard: " + discardpile;
+
     }
 
     public void playcards() {
         numActions -= 1;
-        actions.text = "actions: " + numActions;
+        actions.GetComponent<TMPro.TextMeshProUGUI>().text = "actions: " + numActions;
+        discardpile += 1;
+        discard.GetComponent<TMPro.TextMeshProUGUI>().text = "Discard: " + discardpile;
     }
 
 }

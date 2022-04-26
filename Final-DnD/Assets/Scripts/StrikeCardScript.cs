c23f3e665a5f995919209f84e26c1ecabe538d3d
Superdan56 <71519345+Superdan56@users.noreply.github.com>
2022-04-26 19:01:36 -0400

    started experimenting with Scriptable game objects

--- Final-DnD/Assets/Scripts/StrikeCardScript.cs
+++ Final-DnD/Assets/Scripts/StrikeCardScript.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StrikeCardScript : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+[CreateAssetMenu(fileNme = "newCard", menuName = "Card/Fighter")]
+public class StrikeCardScript : ScriptableObject {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public new string name;
+
 }

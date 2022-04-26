ce4a3df11ecc6f3f6b9bfd7caf5d4df3ac6949dc
Superdan56 <71519345+Superdan56@users.noreply.github.com>
2022-04-26 19:58:45 -0400

    started working with ScriptableObjects

--- Final-DnD/Assets/Scripts/StrikeCardScript.cs
+++ Final-DnD/Assets/Scripts/StrikeCardScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileNme = "newCard", menuName = "Card/Fighter")]
+[CreateAssetMenu(fileName = "newCard", menuName = "Card/Fighter")]
 public class StrikeCardScript : ScriptableObject {
 
     public new string name;

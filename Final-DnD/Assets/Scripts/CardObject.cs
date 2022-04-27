ff6a834ddc9081be5e87a1beded1b7ce740e4f32
Superdan56 <71519345+Superdan56@users.noreply.github.com>
2022-04-27 10:28:14 -0400

    scriptable object created

---
+++ Final-DnD/Assets/Scripts/CardObject.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "newCard", menuName = "Card/playable")]
+public class CardObject : ScriptableObject {
+
+    public new string name;
+
+}

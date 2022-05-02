2f9259525e5ab980ce3e6bc844c72f2146edd3f6
Superdan56 <71519345+Superdan56@users.noreply.github.com>
2022-05-02 18:02:33 -0400

    enemy turns finished

--- Final-DnD/Assets/Scripts/EnemyBehavoir.cs
+++ Final-DnD/Assets/Scripts/EnemyBehavoir.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyBehavoir : MonoBehaviour {
+
+    public GameObject playerHp;
+    public GameObject enemyHp;
     
-    
+    public string[] actionList = {"attack", "heal"};
+    public string randAct;
 
     void Start() {
         
@@ -12,6 +16,12 @@ public class EnemyBehavoir : MonoBehaviour {
 
     // Update is called once per frame
     public void EnemyAction() {
+        randAct = actionList[Random.Range(0, actionList.Length)];
+        if(randAct == "attack") {
 
+        }
+        else {
+            
+        }
     }
 }
